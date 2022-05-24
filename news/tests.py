2fc03482453f30daedd987183b00dfc6c19023e5
Develper-Gitonga <aizensalim52@gmail.com>
2022-05-24 15:03:05 +0300

    commit

--- news/tests.py
+++ news/tests.py
@@ -7,13 +7,13 @@ class EditorTestClass(TestCase):
     
     # Set up method
     def setUp(self):
-        self.james= Editor(first_name = 'James', last_name ='Muriuki', email ='james@moringaschool.com')
+        self.dennis= Editor(first_name = 'Dennis', last_name ='Gitonga', email ='dgitonga@moringaschool.com')
     # Testing  instance
     def test_instance(self):
-        self.assertTrue(isinstance(self.james,Editor))
+        self.assertTrue(isinstance(self.dennis,Editor))
     # Testing Save Method
     def test_save_method(self):
-        self.james.save_editor()
+        self.dennis.save_editor()
         editors = Editor.objects.all()
         self.assertTrue(len(editors) > 0)
 
@@ -22,14 +22,14 @@ class ArticleTestClass(TestCase):
     
     def setUp(self):
         # Creating a new editor and saving it
-        self.james= Editor(first_name = 'James', last_name ='Muriuki', email ='james@moringaschool.com')
-        self.james.save_editor()
+        self.dennis= Editor(first_name = 'Dennis', last_name ='Gitonga', email ='dgitonga@moringaschool.com')
+        self.dennis.save_editor()
 
         # Creating a new tag and saving it
         self.new_tag = tags(name = 'testing')
         self.new_tag.save()
 
-        self.new_article= Article(title = 'Test Article',post = 'This is a random test Post',editor = self.james)
+        self.new_article= Article(title = 'Test Article',post = 'This is a random test Post',editor = self.dennis)
         self.new_article.save()
 
         self.new_article.tags.add(self.new_tag)
