0998586c8f69d8948b3dd68d6cf7bb9d30a55574
Develper-Gitonga <aizensalim52@gmail.com>
2022-05-25 09:26:53 +0300

    commit

--- news/urls.py
+++ news/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns=[
     path('',views.news_of_day,name = 'newsToday'),
-    path('archives/<past_date>/',views.past_days_news,name = 'pastNews')
+    path('archives/<past_date>/',views.past_days_news,name = 'pastNews'),
+    path('search/', views.search_results, name='search_results'),
 ]
