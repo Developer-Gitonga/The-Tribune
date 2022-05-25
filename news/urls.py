fa56dabe9b27f1dc7a44727963f3401257e9914a
Develper-Gitonga <aizensalim52@gmail.com>
2022-05-25 10:22:34 +0300

    commit

--- news/urls.py
+++ news/urls.py
@@ -6,3 +6,5 @@ urlpatterns=[
     path('archives/<past_date>/',views.past_days_news,name = 'pastNews'),
     path('search/', views.search_results, name='search_results'),
 ]
+# if settings.DEBUG:
+#     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
