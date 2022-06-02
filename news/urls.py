d03dcc23f1c442e985b4e718f8fa84e3c2fc7e4a
Develper-Gitonga <aizensalim52@gmail.com>
2022-06-02 10:56:28 +0300

    update

--- news/urls.py
+++ news/urls.py
@@ -8,6 +8,7 @@ urlpatterns=[
     path('archives/<past_date>/',views.past_days_news,name = 'pastNews'),
     path('search/', views.search_results, name='search_results'),
     path('article/<int:id>/' ,views.article,name='article'),
+ 
 ] 
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- tribune/urls.py
+++ tribune/urls.py
@@ -7,6 +7,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('news/',include('news.urls')),
+       path('accounts/', include('registration.backends.simple.urls')),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
